ccec6905bc950a195b55f3e2bcc395e1cf6e9b4e
EnesKaya <130606254+Enesk1999@users.noreply.github.com>
2024-11-24 17:51:35 +0300

    auth side finished

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,7 +54,10 @@ export const signup = async (req, res)=>{
 
         res.status(201).json({
             success:true,
-            user,
+            _id:user._id,
+            name: user.name,
+            email:user.email,
+            role:user.role,
             message:"User created successfully" 
         });
     } catch (error) {
@@ -65,17 +68,89 @@ export const signup = async (req, res)=>{
         })   
     }
 };
-export const login = (req, res)=>{
+export const login =async (req, res)=>{
     try {
-     
+        const {email , password} = req.body;
+        const user = await User.findOne({email});
+        if(user && (await user.comparePassword(password))){
+            const {accessToken , refreshToken} = generateTokens(user._id);
+            await storeRefreshToken(user._id, refreshToken);
+            setCookies(res , accessToken, refreshToken);
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            });
+            console.log("here is works")
+        }
+
     } catch (error) {
-        
+        console.log("Error in login Auth Controller")
+        res.status(500).json({
+            success: false,
+            message:"Internal Server Error",
+        })  
     }
 }
-export const logout = (req, res)=>{
+export const logout = async (req, res)=>{
     try {
+        const refreshToken = req.cookies.refreshToken
+        if(refreshToken){
+            const decoded = jwt.verify(refreshToken , process.env.REFRESH_TOKEN_SECRET);
+            await redis.del(`refresh_token:${decoded.userId}`)
+        }
+
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
         
+        res.json({mesage: "Logged out successfully"})
     } catch (error) {
-        
+        console.log("Error in Auth Controller")
+        res.status(500).json({
+            success: false,
+            message:"Internal Server Error"
+        })   
     }
 }
+
+export const refreshToken =async (req, res) =>{
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if(!refreshToken){
+            return res.status(401).json({message:"No refresh token provided"});
+        };
+
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
+        if(storedToken !== refreshToken){
+            return res.status(401).json({message:"Invalid refresh token"});
+        }
+
+        const accessToken = jwt.sign({userId:decoded.userId} , process.env.ACCESS_TOKEN_SECRET, {expiresIn:"15m"});
+
+        res.cookie("accessToken",accessToken,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 15 * 60 * 60,
+        });
+        res.json({
+            message: "Token refreshed successfully",
+        })
+    } catch (error) {
+        console.log("Error in refreshToken Auth Controller")
+        res.status(500).json({
+            success: false,
+            message:"Internal Server Error"
+        })   
+    }
+};
+
+// export const getProfile= async(req,res) =>{
+//     try {
+        
+//     } catch (error) {
+        
+//     }
+// } 
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express, { json } from "express";
 import dotnev from "dotenv";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 import { connectDatabase } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 dotnev.config();
     
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());    //request the client UI side get for information
-
-app.use("/api/auth", authRoutes)
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes)
 
 app.listen(PORT ,()=>{
     console.log(`Server is running on port: http://localhost:${PORT}`)
